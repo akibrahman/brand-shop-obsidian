0f423a35f20fef169ec78e54fef7778a2f50e669
akibrahman <akibrahman5200@gmail.com>
2023-10-21 18:07:57 +0600

    Banner's Design of this website is updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,27 @@
-import banner from "/Banner.jpg";
+import { useLoaderData } from "react-router-dom";
 
 const Banner = () => {
+  const brands = useLoaderData();
   return (
-    <div className="border-b">
-      <img src={banner} className="w-full" alt="" />
+    <div className="border rounded-lg flex items-center justify-between w-[90%] mx-auto p-20 mt-10">
+      <div className="">
+        <p className="text-8xl font-semibold leading-[100px] italic">
+          <span className={`text-orange-400`}>New</span> <br /> Collection{" "}
+          <br />{" "}
+          <span>
+            <span className={`text-orange-400`}>50%</span> off
+          </span>
+        </p>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {brands.map((brand) => (
+          <img
+            className="w-40 bg-[#202126] p-4 rounded-lg"
+            key={brand._id}
+            src={brand.BrandImage}
+          />
+        ))}
+      </div>
     </div>
   );
 };
